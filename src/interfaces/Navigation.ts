6a00db92562bd972164b1f493c787aad3ca3c564
LauColombini <colombini.lautaro@gmail.com>
2023-09-12 01:38:20 -0300

    feat: update interfaces

--- src/interfaces/Navigation.ts
+++ src/interfaces/Navigation.ts
@@ -3,7 +3,8 @@ import { NavigatorScreenParams } from "@react-navigation/native";
 export type HomeNavigation = {
   Home: undefined;
 };
-
+export type InfoNavigation = {};
 export type TabBarNavigation = {
   HomeTab: NavigatorScreenParams<HomeNavigation>;
+  InfoTab: NavigatorScreenParams<InfoNavigation>;
 };
