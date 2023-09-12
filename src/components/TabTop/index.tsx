3059f4a2baef6fdcf0c692df68c8fab5cf9bf9c8
LauColombini <colombini.lautaro@gmail.com>
2023-09-12 02:56:19 -0300

    feat: android fixes

--- src/components/TabTop/index.tsx
+++ src/components/TabTop/index.tsx
@@ -1,12 +1,22 @@
-import { View, Text } from "react-native";
+import { View, Platform, Image } from "react-native";
 import React from "react";
 import LogoIcon from "../../assets/svg/LogoIcon";
+import LogoIconPng from "../../assets/img/Logo.png";
 import styles from "./styles";
 
 const TabTop = () => {
   return (
     <View style={[styles.tabContainer]}>
-      <LogoIcon />
+      {Platform.OS === "ios" && <LogoIcon />}
+      {Platform.OS === "android" && (
+        <Image
+          source={LogoIconPng}
+          style={{
+            width: 120,
+            height: 20,
+          }}
+        />
+      )}
     </View>
   );
 };
